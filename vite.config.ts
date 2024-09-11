406178063f9e9c3293fba0eaa3b25f7d6299ab57
DESKTOP-A742PF6\default <jinseok1006@naver.com>
2024-09-11 12:47:51 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portal/',
+  // base: '/portal/',
   plugins: [react()],
   server: {
     host: true
