13e438f517c7f48a5204d3b88a88210ff51522fa
DESKTOP-A742PF6\default <jinseok1006@naver.com>
2024-09-12 12:07:16 +0900

    realm명 수정

--- src/main.tsx
+++ src/main.tsx
@@ -53,7 +53,7 @@ theme = createTheme(theme, {
 });
 
 const oidcConfig = {
-  authority: `${import.meta.env.VITE_KEYCLOAK_URL}/realms/jedutools`,
+  authority: `${import.meta.env.VITE_KEYCLOAK_URL}/realms/JEduTools`,
   client_id: "jedutools-portal",
   redirect_uri: `${window.location.origin}${window.location.pathname}`,
   post_logout_redirect_uri: window.location.origin,
