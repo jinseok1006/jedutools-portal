9389ec2185396ec9e5e12e9bdac5cd649369e85c
DESKTOP-A742PF6\default <jinseok1006@naver.com>
2024-09-12 14:57:46 +0900

    magiclink 로그인시 자동으로 로그인하도록 수정

--- src/hooks/useKeycloak.ts
+++ src/hooks/useKeycloak.ts
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth, hasAuthParams } from "react-oidc-context";
 
 export default function useKeycloak() {
   const auth = useAuth();
   const [hasTriedSignin, setHasTriedSignin] = useState(false);
 
+  useEffect(() => {
+    if (hasAuthParams()) {
+      auth.signinSilent();
+    }
+  }, []); // TODO: 이메일 로그인 이후 state 제거 방법 생각
+
   const handleLogin = async () => {
     if (
       !(
-        hasAuthParams() ||
         auth.isAuthenticated ||
         auth.activeNavigator ||
         auth.isLoading ||
